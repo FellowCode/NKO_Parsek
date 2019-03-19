56dfa0f0b92e153b1d0c8cc50b437e8a2ebba9d3
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-03-19 12:35:58 +1000

    fix admin change news

--- News/admin.py
+++ News/admin.py
@@ -9,9 +9,18 @@ from django.utils.html import format_html
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
+    change_form_template = 'custom_admin/change_news.html'
+
     readonly_fields = ['date_edit']
     def response_change(self, request, obj):
         default_page = super().response_change(request, obj)
+        if '_preview' in request.POST:
+            return redirect('/news/?id=%d'%obj.id)
+        else:
+            return default_page
+
+    def response_add(self, request, obj, post_url_continue=None):
+        default_page = super().response_add(request, obj, post_url_continue)
         if '_preview' in request.POST:
             return redirect('/news/?id=%d'%obj.id)
         else:
