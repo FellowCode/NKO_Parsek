c22f3c118d413522f773348fa55574ef4a8c97a9
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-03-20 00:13:29 +1000

    fix image in news body

--- static/js/news.js
+++ static/js/news.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.materialboxed').materialbox();
+
+    $('.body .material-placeholder').each(function (index) {
+        var img_block = $(this);
+        var img = img_block.children().first();
+        if (!img.hasClass("float") || img.hasClass("i-center"))
+            img_block.wrap("<div class='image-inline'></div>");
+        img_block.addClass(img.attr('class'));
+        img_block.removeClass('materialboxed');
+    });
 });
