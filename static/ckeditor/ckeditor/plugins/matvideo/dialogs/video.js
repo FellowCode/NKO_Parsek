944dcbebad285556dd7236bf753c59b0922ee032
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-03-20 00:02:04 +1000

    update matvideo

--- static/ckeditor/ckeditor/plugins/matvideo/dialogs/video.js
+++ static/ckeditor/ckeditor/plugins/matvideo/dialogs/video.js
@@ -2,12 +2,19 @@ CKEDITOR.dialog.add( 'videoDialog', function( editor ) {
     return {
         title: 'Добавить видео',
         minWidth: 400,
-        minHeight: 100,
+        minHeight: 120,
         contents: [
             {
                 id: 'tab-basic',
                 label: 'Добавить видео',
                 elements: [
+                    {
+                        type: 'select',
+                        id: 'video-host',
+                        label: 'Видеохостинг',
+                        items: [['YouTube']],
+                        'default': 'YouTube',
+                    },
                     {
                         type: 'text',
                         id: 'url',
@@ -23,10 +30,14 @@ CKEDITOR.dialog.add( 'videoDialog', function( editor ) {
             var video = editor.document.createElement( 'div' );
             video.addClass('video-container');
 
+            var url = dialog.getValueOf('tab-basic', 'url');
+            i = url.indexOf('watch?v=');
+            url = url.slice(0, i) + 'embed/' + url.slice(i+8);
+            console.log(url);
             var video_frame = editor.document.createElement( 'iframe' );
-            video_frame.setAttribute('src', dialog.getValueOf('tab-basic', 'url'));
-            video_frame.setAttribute('width', dialog.getValueOf('tab-basic', '640'));
-            video_frame.setAttribute('height', dialog.getValueOf('tab-basic', '480'));
+            video_frame.setAttribute('src', url);
+            video_frame.setAttribute('width', '640');
+            video_frame.setAttribute('height','480');
             video_frame.setAttribute('frameborder', 0);
             video_frame.setAttribute('allowfullscreen', '');
             video.append(video_frame);
